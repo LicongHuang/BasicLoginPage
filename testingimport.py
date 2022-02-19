3e9c8788b53dc227f4c6b6dfe018c607356b6c4d
LicongHuang <alexliconghuang@gmail.com>
2022-02-19 15:04:42 +0800

    idk if this is new

--- testingimport.py
+++ testingimport.py
@@ -1,2 +1,17 @@
-import sqlalchemy
+import sqlalchemy as db
+
+engine = db.create_engine('mysql+pymysql://root:@localhost/login')
+connection = engine.connect()
+metadata = db.MetaData()
+
+census = db.Table('login_info', metadata,autoload=True,autoload_with=engine)
+
+query = db.select([census])
+
+ResultProxy = connection.execute(query)
+
+ResultSet = ResultProxy.fetchall()
+
+print(ResultSet[:3])
+
 
